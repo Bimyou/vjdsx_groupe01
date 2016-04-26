f9aa9e7baeb040e1a634889ef121b7b56802f5b7
rouyer <paul.rouyer55@gmail.com>
2016-04-26 15:50:48 +0200

    Interface DAO

---
+++ Lynx/src/main/java/intiformation/javajee/lynx/dao/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author Charef Xavier
+ * nomProjet : Lynx
+ * 
+ * date 26/04/2016
+ * Package : intiformation.javajee.lynx.dao
+ * ref-Uml:1
+ * version 1
+ * Sprint : 1
+ * 
+ * 
+ */
+package intiformation.javajee.lynx.dao;
